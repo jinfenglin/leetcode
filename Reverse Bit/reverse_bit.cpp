3b925c74cd13ffc20edad4dc077b6781ea1f2113
jinfenglin <ljfnwpusife@gmail.com>
2015-05-08 23:13:40 -0400

    first commit

---
+++ Reverse Bit/reverse_bit.cpp
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+      return 0;  
+    }
+};
+
+int main()
+{
+	Solution *sol=new Solution();
+	cout<<"Done"<<endl;
+	return 0;	
+}
