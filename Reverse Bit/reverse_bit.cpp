e0165aded7f756986ffc343882bc22805c4c1447
jinfenglin <ljfnwpusife@gmail.com>
2015-05-10 14:55:22 -0400

    subset

--- Reverse Bit/reverse_bit.cpp
+++ Reverse Bit/reverse_bit.cpp
@@ -1,16 +1,27 @@
 #include <stdint.h>
 #include <iostream>
+#include <cassert>
 using namespace std;
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-      return 0;  
+	    uint32_t result=0;
+	    for(int i=0;i<32;i++)
+	    {
+		    uint32_t temp_bit=n & 1;
+		    n>>=1;
+		    result<<=1;
+		    result= result | temp_bit;
+	    }
+	    cout<<result<<endl;
+	    return result;  
     }
 };
 
 int main()
 {
 	Solution *sol=new Solution();
-	cout<<"Done"<<endl;
+
+	//cout<<"Done"<<endl;
 	return 0;	
 }
---
+++ Subset/subset.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+static void display_vec(vector<vector<int> > result){
+	cout<<"vec in vec"<<endl;
+	for(vector<vector<int> >::iterator it=result.begin();it!=result.end();it++){
+		for(vector<int>::iterator sub_it=(*it).begin();sub_it!=(*it).end();sub_it++)
+		{
+			cout<<*sub_it<<" ";
+		}
+		cout<<endl;
+	}
+}
+static  void display_vec(vector<int> result){
+	cout<<"-----------------"<<endl;
+	for(vector<int>::iterator it=result.begin();it!=result.end();it++)
+	{
+		cout<<*it<<" ";
+	}
+	cout<<endl;
+}
+class Solution{
+	public:
+		vector<vector<int> > subsets(vector<int>& nums) {
+			if (nums.size()==0)
+				return vector< vector<int> >(1,vector<int>());
+			int first=nums.front();			
+			nums.erase(nums.begin());
+			vector<vector<int> > combin=subsets(nums);
+			vector<vector<int> > temp;
+			for(vector<vector<int> >::iterator it=combin.begin();it!=combin.end();it++){
+				vector<int> one_combin=*it;
+				one_combin.insert(one_combin.begin(),first);
+				temp.push_back(one_combin);
+			}
+			for(vector<vector<int> >::iterator it=temp.begin();it!=temp.end();it++)
+			{
+				combin.push_back(*it);
+			}
+			for(vector<vector<int> >::iterator it=combin.begin();it!=combin.end();it++)
+			{
+				sort((*it).begin(),(*it).end());
+			}
+			display_vec(combin);
+			return combin;
+		}
+};
+
+int main()
+{
+	const int array[]={4,1,0};
+	vector<int> init(array,array+sizeof(array)/sizeof(int));
+	Solution *sol= new Solution();
+	vector<vector<int> > result=sol->subsets(init);
+	//display_vec(result);
+	return 0;
+}
