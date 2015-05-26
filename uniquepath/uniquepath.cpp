0ebb066a1db053983bacf656891603f165d9908f
jinfenglin <ljfnwpusife@gmail.com>
2015-05-26 12:38:27 -0400

    unque path

--- uniquepath/uniquepath.cpp
+++ uniquepath/uniquepath.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-	    if(m==1 or n==1)
-		    return 1;
-	    int part1=uniquePaths(m-1,n);
-	    int part2=uniquePaths(m,n-1);
-	    return part1+part2;
-        
+	    int *mem=(int*)malloc(m*n*sizeof(int));
+	    for(int i=0;i<n;i++){
+	    	*(mem+i)=1;
+	    }
+	    for(int i=1;i<m;i++)
+	    {
+		    *(mem+i*n)=1;
+		    for(int j=1;j<n;j++){
+			    //mem[i][j]=m[i-1][j]+m[i][j-1];
+			    *(mem+i*n+j)=*(mem+(i-1)*n+j)+*(mem+i*n+j-1);
+		    
+		    }
+	    }
+	    return *(mem+(m-1)*n+n-1);
+    	    
     }
 };
 int main(){
 	Solution *sol=new Solution;
-	cout<<sol->uniquePaths(2,2)<<endl;
+	//sol->uniquePaths(2,2);
+	cout<<sol->uniquePaths(3,3)<<endl;
 }
