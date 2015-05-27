5fdc52de7d49e3b2905a433d65b3852c7d46a5c6
jinfenglin <ljfnwpusife@gmail.com>
2015-05-27 11:06:13 -0400

    new try

--- Trapping Rain Water/trappingrainwater.cpp
+++ Trapping Rain Water/trappingrainwater.cpp
@@ -1,34 +1,62 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 class Solution {
 	public:
-		int trap(vector<int>& height) {
-			int max=0;
+		int cal_water(stack<int> small_hill){
+			if(small_hill.empty())
+				return 0;
 			int water=0;
-			for(int x:height)
-				if(x>max)
-					max=x;
-			for(int i=0;i<max;i++){
-				int start=0;
-				for(int j=0;j<height.size();j++){
-					if(height[j]==0)
-					{
-						if(j>0 and height[j-1]>0)
-							start=j-1;
-						if(j+1<height.size() and height[j+1]>0)
-						{
-							water+=j-start;
-						}
-					}		
-				}
+			int start=small_hill.top();
+			while(!small_hill.empty()){
+				if(start>small_hill.top())
+					water+=start-small_hill.top();
+				small_hill.pop();
 			}
+			cout<<water<<endl;
+			return water;
+		};
+		int trap(vector<int>& height) {
+			if(height.size()<3)
+				return 0;
+			stack<int> hill;
+			int water=0;
+			int bottom=0;
+			for(int i=0;i<height.size();i++){
+				if(hill.empty() and height[i]==0)
+					continue;
+				else if(hill.empty()){
+					bottom=height[i];
+					hill.push(height[i]);
+					continue;
+				}
+				stack<int> small_hill;
+				if(height[i]>bottom){
+					while(!hill.empty()){
+						small_hill.push(hill.top());
+						hill.pop();
+					}
+					water+=cal_water(small_hill);
+					bottom=height[i];
+				}
+
+				hill.push(height[i]);
+			}
+			vector<int> new_iter;
+			while(!hill.empty())
+			{
+				new_iter.push_back(hill.top());
+				hill.pop();
+			}
+			water+=trap(new_iter);
 			return water;
 		}
 };
 int main(){
 	Solution *sol= new Solution;
-	//vector<int> init={0,1,0,2,1,0,1,3,2,1,2,1};
-	vector<int> init={};
+	vector<int> init={0,1,0,2,1,0,1,3,2,1,2,1};
+	//vector<int> init={0,1,0,2,1,0,1,3};
+	//vector<int> init={};
 	cout<<sol->trap(init)<<endl;
 }
