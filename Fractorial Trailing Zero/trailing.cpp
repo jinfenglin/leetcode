1b3b913aba02e95a561b87b0d1d7daa9e46c3da6
jinfenglin <ljfnwpusife@gmail.com>
2015-09-20 17:00:11 -0400

    trailing

---
+++ Fractorial Trailing Zero/trailing.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class Solution {
+	public:
+		int trailingZeroes(int n) {
+			int count=0;
+			while(n/=5) count+=n;			
+			return count;
+		}
+};
+int main(){
+}
