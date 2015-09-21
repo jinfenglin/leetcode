ec7f97c05f0618aea6467e8daf91d5611bada307
jinfenglin <ljfnwpusife@gmail.com>
2015-09-21 13:22:32 -0400

    largest num

---
+++ Largest Number/largest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Solution {
+	public:	
+		string largestNumber(vector<int>& nums) {
+			vector<string> num_string;
+			string res="";
+			for(int num:nums){
+				num_string.push_back(to_string(num));
+			}
+			sort(num_string.begin(),num_string.end(),[](const string &m,const string &n){ return m+n > n+m; });
+			for(string str:num_string){
+				res+=str;
+			}
+			return res;
+		}
+};
+int main(){
+}
