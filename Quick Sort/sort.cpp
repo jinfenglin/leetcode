47613fb8f3188a07d247a065e76028b0b06f52db
jinfenglin <ljfnwpusife@gmail.com>
2015-10-18 23:53:59 -0400

    quick sort

--- Quick Sort/sort.cpp
+++ Quick Sort/sort.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int patition(int *array,int start,int end);
 void sort(int start,int end,int *array);
-int patition(int *array,int start,int end){
+/*int patition(int *array,int start,int end){
 	//cout<<end<<endl;
-	/*int k=(start+end)/2;
+	int k=(start+end)/2;
 	int pivot=array[k];
 	while(start<end){
 		while(start<k and array[start]<=pivot)
@@ -20,15 +20,26 @@ int patition(int *array,int start,int end){
 		cout<<array[i]<<" ";
 	cout<<endl;
 
-	return end;*/
+	return end;
+}*/
 
-
-	int pivot
+int patition(int *array,int start,int end){
+	int pivot=array[end];
+	while(start<end){
+		while(array[start]<=array[end] and start<end)
+			start++;
+		swap(array[start],array[end]);
+		while(array[end]>=array[start] and start<end)
+			end--;
+		swap(array[start],array[end]);
+	}
+	return end;
 }
 void sort(int start,int end,int *array){
 	if(start>=end)
 		return;
 	int point=patition(array,start,end);
+	cout<<point<<endl;
 	sort(start,point-1,array);
 	sort(point+1,end,array);
 }
