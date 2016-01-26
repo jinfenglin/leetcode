89093a74c257e949829300d340d785f6db8a281b
jinfenglin <ljfnwpusife@gmail.com>
2016-01-26 13:34:50 -0500

    slightly faster

--- Maximum Product Subarray/Solution.java
+++ Maximum Product Subarray/Solution.java
@@ -1,16 +1,22 @@
 public class Solution {
 	public int maxProduct(int[] nums) {
-		int[] max,min;
+		int max,min;
 		if(nums.length==0)
 			return 0;
-		max=new int[nums.length];
-		min=new int[nums.length];
-		max[0]=nums[0];
-		min[0]=nums[0];
+		max=nums[0];
+		min=nums[0];
+		int res=nums[0];
 		for(int i=1;i<nums.length;i++){
-			max[i]=Math.max(max[i-1],Math.max(min[i-1]*nums[i],Math.max(max[i-1]*nums[i],nums[i])));
-			min[i]=Math.min(min[i-1],Math.min(min[i-1]*nums[i],Math.min(min[i-1]*nums[i],nums[i])));
+			int tmpS,tmpL;
+			tmpL=max*nums[i];
+			tmpS=min*nums[i];
+			max=Math.max(tmpL,tmpS);
+			min=Math.min(tmpL,tmpS);
+			max=Math.max(nums[i],max);
+			min=Math.min(nums[i],min);
+			res=Math.max(res,max);
+
 		}
-		return max[nums.length-1];
+		return res;
 	}
 }
