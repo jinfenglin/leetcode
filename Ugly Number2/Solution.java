0a28fe2818b59002f09c24e82a571272ca8fec24
jinfenglin <ljfnwpusife@gmail.com>
2016-02-19 13:07:47 -0500

    ugly numbers2

---
+++ Ugly Number2/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+	public int nthUglyNumber(int n) {
+		int c1=1,c2=1,c3=1;
+		List<Integer> uglys=new ArrayList<Integer>();
+		int min=1;
+		for(int i=1;i<n;i++){
+			int n1=uglys.get(c1)*2;
+			int n2=uglys.get(c2)*3;
+			int n3=uglys.get(c3)*5;
+			min=Math.min(n3,Math.min(n1,n2));
+			if(min==n1)
+				c1++;
+			if(min==n2)
+				c2++;
+			if(min==n3)
+				c3++;
+			uglys.add(min);
+		}
+		return min;
+	}
+}
